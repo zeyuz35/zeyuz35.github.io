f79bc2ba29ec3fa3ca8650ec5b102af1c7e17914
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-06-22 23:13:47 +0000

    Refactor pandas iterrows to itertuples for faster markdown generation

--- markdown_generator/talks.py
+++ markdown_generator/talks.py
@@ -64,7 +64,8 @@ def html_escape(text):
 
 loc_dict = {}
 
-for row, item in talks.iterrows():
+# OPTIMIZATION: Replacing iterrows() with itertuples() for significantly faster iteration over pandas DataFrames
+for item in talks.itertuples():
     
     md_filename = str(item.date) + "-" + item.url_slug + ".md"
     html_filename = str(item.date) + "-" + item.url_slug 
